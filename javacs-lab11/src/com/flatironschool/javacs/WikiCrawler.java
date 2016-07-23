6be1f8dd72ed667e24b5c846fcc7016802f679c2
christian <chrissantos1995@live.com>
2016-07-23 12:52:59 -0400

    Done.

--- javacs-lab11/src/com/flatironschool/javacs/WikiCrawler.java
+++ javacs-lab11/src/com/flatironschool/javacs/WikiCrawler.java
@@ -8,6 +8,7 @@ import java.util.Queue;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.jsoup.nodes.Node;
 
 import redis.clients.jedis.Jedis;
 
@@ -54,8 +55,33 @@ public class WikiCrawler {
 	 * @throws IOException
 	 */
 	public String crawl(boolean testing) throws IOException {
-        // FILL THIS IN!
-		return null;
+
+		if(queue.isEmpty())
+			return null;
+
+		// URL to crawl
+		String url = queue.poll();
+
+		System.out.println("Checking URL: " + url);
+
+		// Elements to iterate through
+		Elements paragraphs;
+
+		if(testing)
+			paragraphs = wf.readWikipedia(url);
+		else
+			paragraphs = wf.fetchWikipedia(url);
+
+		// Index the page if testing or if not already indexed
+		if(testing || !index.isIndexed(url))
+			index.indexPage(url, paragraphs);
+		else
+			return null;
+
+		// Queue the internal links of the page
+		queueInternalLinks(paragraphs);
+
+		return url;
 	}
 	
 	/**
@@ -65,8 +91,42 @@ public class WikiCrawler {
 	 */
 	// NOTE: absence of access level modifier means package-level
 	void queueInternalLinks(Elements paragraphs) {
-        // FILL THIS IN!
+       
+		// Search for internal link
+		for(Element p : paragraphs) {
+
+			for(Element el : p.getAllElements()) {
+
+				if(isInternalLink(el)) {
+
+					//System.out.println("Found link!");
+
+					// Add internal link to the queue
+					String urlToQueue = el.attr("href");
+					if(!urlToQueue.contains("https://en.wikipedia.org")) {
+						urlToQueue = "https://en.wikipedia.org" + urlToQueue;
+					}
+
+					queue.offer(urlToQueue);
+				}
+			}
+		}
 	}
+
+	boolean isInternalLink(Element el) {
+
+		//System.out.println("Checking if el is link");
+
+		//System.out.println("\tel has tagname: " + el.tagName());
+		//System.out.println("\tel has link: " + el.attr("href"));
+		//System.out.println("\tel link contains wiki/: " + el.attr("href").contains("wiki/"));
+
+		boolean isLink = el.tagName().equals("a");
+		boolean isInternalLink = isLink && el.attr("href").contains("wiki/");
+
+		// Check if element is anchor tag pointing to a wikipedia page
+		return isInternalLink;
+	}	
 
 	public static void main(String[] args) throws IOException {
 		
@@ -86,7 +146,6 @@ public class WikiCrawler {
 			res = wc.crawl(false);
 
             // REMOVE THIS BREAK STATEMENT WHEN crawl() IS WORKING
-            break;
 		} while (res == null);
 		
 		Map<String, Integer> map = index.getCounts("the");
